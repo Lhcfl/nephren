7e136bedcc5cc029ae89ded110844383d6ea0eed
linca <lhcfl@outlook.com>
2026-06-25 11:59:59 +0800

    aaa

--- src/share/vmess.rs
+++ src/share/vmess.rs
@@ -1,4 +1,4 @@
-use anyhow::Context;
+use anyhow::{Context, bail};
 use base64::prelude::*;
 use log::{debug, error};
 /// vless share link parser
@@ -10,13 +10,15 @@ use crate::models::{
     protocol::{
         Protocol,
         vless::{Config, Flow},
-        vmess::{self, Security},
+        vmess,
     },
-    transport::StreamSettings,
+    security,
+    transport::{self, StreamSettings, Transport},
 };
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct VMessShare {
+    v: String,
     ps: String,
     #[serde(rename = "add")]
     address: String,
@@ -25,27 +27,60 @@ struct VMessShare {
     #[serde(rename = "aid")]
     alter_id: String,
     #[serde(rename = "scy")]
-    security: vmess::Security,
+    vmess_security: vmess::Security,
+    net: String,
+    #[serde(rename = "type")]
+    transport_security: String,
+    host: String,
+    path: String,
+    tls: String,
+    sni: String,
+    alpn: String,
+    #[serde(rename = "fp")]
+    fingerprint: String,
+    insecure: String,
 }
 
 pub fn parse_vmess_url(url: &Url) -> anyhow::Result<Node> {
-    error!("todo: parse transport and security from url");
-    let transport = StreamSettings::default();
-
     let input = url.domain().context("no domain")?;
     let decoded = BASE64_STANDARD.decode(input)?;
     let decoded_str = String::from_utf8(decoded)?;
 
-    debug!("{}", decoded_str);
-
     let VMessShare {
+        v,
         ps,
         address,
         port,
         id,
         alter_id,
-        security,
+        vmess_security,
+        net,
+        transport_security,
+        host,
+        path,
+        tls,
+        sni,
+        alpn,
+        fingerprint: fp,
+        insecure,
     } = serde_json::from_str(&decoded_str)?;
+
+    if v != "2" {
+        bail!("`v` should equals 2. the decoded str is {decoded_str}")
+    }
+
+    let transport = match net.as_str() {
+        "tcp" => Transport::Tcp(transport::tcp::Config {
+            accept_proxy_protocol: false,
+            header: None,
+        }),
+        x => bail!("not implemented: {x} {decoded_str}"),
+    };
+
+    let security = match transport_security.as_str() {
+        "none" => security::Security::None,
+        x => bail!("not implemented: {x} {decoded_str}"),
+    };
 
     let config = vmess::Config {
         address,
@@ -53,7 +88,7 @@ pub fn parse_vmess_url(url: &Url) -> anyhow::Result<Node> {
         id,
         alter_id: alter_id.parse()?,
         level: None,
-        security,
+        security: vmess_security,
     };
 
     Ok(Node {
@@ -61,6 +96,9 @@ pub fn parse_vmess_url(url: &Url) -> anyhow::Result<Node> {
         name: ps,
         belongs_to: None,
         protocol: Protocol::VMess(config),
-        transport,
+        transport: StreamSettings {
+            transport,
+            security,
+        },
     })
 }
